24430bd08bf248cb1534976ab7ba7c309adc1e46
Anna Wróbel <anna-wro@users.noreply.github.com>
2017-05-10 19:41:27 +0200

    :full_moon: Twig w tabelkach

---
+++ bookmarks - twig tables/web/index.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Comment put here...
+ */
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', true);
+
+require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
+require_once dirname(dirname(__FILE__)) . '/src/templates/templates.inc.php';
+
+use Silex\Application;
+use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Model\Bookmarks\Arr\Bookmarks;
+
+$app = new Silex\Application();
+$app['debug'] = true;
+$app->register(
+    new TwigServiceProvider(),
+    [
+        'twig.path' => dirname(dirname(__FILE__)) . '/templates',
+    ]
+);
+
+$bookmarksModel = new Bookmarks();
+$view = [];
+
+$app->get(
+    '/',
+    function () use ($app) {
+        return '';
+    }
+);
+
+$app->get(
+    '/hello/{name}',
+    function ($name) use ($app) {
+        return $app['twig']->render(
+            'hello/index.html.twig', // nazwa szablonu
+            ['name' => $name] // tablice parametrow przekazywanych do silnika szablonow
+        );
+    }
+);
+
+$app->get(
+    '/bookmark/',
+    function () use ($app, $bookmarksModel) {
+        return $app['twig']->render(
+            'bookmark/index.html.twig',
+            ['bookmarks' => $bookmarksModel->findAll()]
+        );
+    }
+);
+
+$app->get(
+    '/bookmark/{id}',
+    function ($id) use ($app, $bookmarksModel) {
+        return $app['twig']->render(
+            'bookmark/view.html.twig',
+            ['bookmark' => $bookmarksModel->findOneById($id),
+                'bookmarkId' => $id]
+        );
+    }
+);
+
+$app->run();
