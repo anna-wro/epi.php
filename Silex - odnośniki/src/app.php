4bcf894a134ef8cfc1b5964bdf3e8dc64c64fbcb
Anna Wróbel <anna-wro@users.noreply.github.com>
2017-05-16 14:24:06 +0200

    Add files via upload

---
+++ Silex - odnośniki/src/app.php
@@ -0,0 +1,39 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\LocaleServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+
+$app = new Application();
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new AssetServiceProvider());
+$app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+$app['twig'] = $app->extend('twig', function ($twig, $app) {
+    // add custom globals, filters, tags, ...
+
+    return $twig;
+});
+
+$app->register(new LocaleServiceProvider());
+$app->register(
+    new TranslationServiceProvider(),
+    [
+        'locale' => 'pl',
+        'locale_fallbancks' => array('en'),
+    ]
+);
+$app->extend('translator', function ($translator, $app) {
+    $translator->addResource('xliff', __DIR__.'/../translations/messages.en.xlf', 'en', 'messages');
+    $translator->addResource('xliff', __DIR__.'/../translations/validators.en.xlf', 'en', 'validators');
+    $translator->addResource('xliff', __DIR__.'/../translations/messages.pl.xlf', 'pl', 'messages');
+    $translator->addResource('xliff', __DIR__.'/../translations/validators.pl.xlf', 'pl', 'validators');
+
+    return $translator;
+});
+
+return $app;
